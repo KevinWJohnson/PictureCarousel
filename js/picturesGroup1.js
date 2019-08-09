86a8723d869ae760992faab3652b8dad5294d632
KevinWJohnson <kevin.johnson3@cox.net>
2019-08-09 16:25:20 -0700

    Started on carousel

---
+++ js/picturesGroup1.js
@@ -0,0 +1,27 @@
+window.PicGrp1 = (function () {
+
+  const pictures = [
+    {
+      title: 'Ocotillo1',
+      author: 'John Smith',
+      imageUrl: 'images/group1/IMG_0059.JPG',
+    },
+    {
+        title: 'Ocotillo2',
+        author: 'Jane Johnson',
+        imageUrl: 'images/group1/IMG_0062.JPG',
+    },
+    {
+        title: 'Catus Flower',
+        author: 'Mark Benson',
+        imageUrl: 'images/group1/IMG_0064.JPG',
+    },
+    {
+        title: 'Mountains',
+        author: 'Maria Lopez',
+        imageUrl: 'images/group1/IMG_0095.JPG',
+    },
+  ];
+
+  return { pictures: pictures };
+}());
